e3fa2d2c30c1a9b4f12d1b981bb19a5cd6b009de
Nick Bradley <nick.bradley.193@pm.me>
2018-04-13 05:59:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,10 +7,24 @@ function takeANumber(katzDeliLine, customerName) {
 
 function nowServing(katzDeliLine) {
   if (katzDeliLine.length > 0) {
-    var currentCustomer = katzDeliLine[0];  
-    
     return `Currently serving ${katzDeliLine.shift()}.`;
   } else {
     return 'There is nobody waiting to be served!';
   }
+}
+
+function currentLine(katzDeliLine) {
+  var returnString;
+  if (katzDeliLine.length > 0) {
+    returnString = 'The line is currently: ';
+    for (var n = 0; n < katzDeliLine.length; n++) {
+      if (n > 0) {
+        returnString = returnString + ', ';
+      }
+      returnString = returnString + '${n+1}. ${katzDeliLine[n]}';
+    }
+  } else {
+    returnString = 'The line is currently empty.';
+  }
+  return returnString;
 }
